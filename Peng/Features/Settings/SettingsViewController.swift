ae1bcf6487853b31851b94e76223f23f588d152f
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-10-14 22:24:22 +0200

    added logout button to settings

---
+++ Peng/Features/Settings/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Peng
+//
+//  Created by Yannick Schuchmann on 14.10.15.
+//  Copyright © 2015 Yannick Schuchmann. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class SettingsViewController: UIViewController, FBSDKLoginButtonDelegate {
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        self.performSegueWithIdentifier("loggedOut", sender: self)
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        
+    }
+    
+}
