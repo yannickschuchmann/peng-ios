a9fc7e6f888c8ff5979aadd5d54246553bdfc6e4
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-10-26 14:51:21 +0100

    added duelview not optimal for iphone 4

---
+++ Peng/Features/NewDuel/NewDuelViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewDuelViewController.swift
+//  Peng
+//
+//  Created by Yannick Schuchmann on 26.10.15.
+//  Copyright © 2015 Yannick Schuchmann. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewDuelViewController: UIViewController {
+    
+}
